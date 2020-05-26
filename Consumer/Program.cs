fed27c97153f87bcb8c75aa07baee7e95fe75f68
Nikishkin Sergey <nikishkinsy@gmail.com>
2020-05-26 14:06:32 +0300

    init

---
+++ Consumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://root:root@rabbitmq:5672");
+            var exchangeName = "direct.exchange";
+            var queueName = "queue";
+
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                channel.QueueDeclare(queueName, true, false, false);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (ch, ea) =>
+                {
+                    var str = System.Text.Encoding.UTF8.GetString(ea.Body.ToArray());
+                    Console.WriteLine(str);
+                };
+                channel.BasicConsume(queueName, false, consumer);
+
+                while (true)
+                {
+                    Task.Delay(1000).Wait();
+                }
+                //while (Console.ReadKey().Key != ConsoleKey.Escape)
+                //{
+                //}
+            }
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://root:root@rabbitmq:5672");
+            var exchangeName = "direct.exchange";
+            var queueName = "queue";
+
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+                channel.QueueDeclare(queueName, true, false, false);
+
+                var counter = 0;
+                do
+                {
+                    var msg = $"counter: {counter++}";
+                    var messageBodyBytes = System.Text.Encoding.UTF8.GetBytes(msg);
+                    channel.BasicPublish(string.Empty, queueName, body: messageBodyBytes);
+                    Console.WriteLine(msg);
+                    Task.Delay(1000).Wait();
+                }
+                while (true);
+                //while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+        }
+    }
+}
